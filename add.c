09932aa61c21cfdd5c9944402cc7e5f6d1e62c60
shahbasharat <shahbasharat577@outlook.com>
2021-11-09 01:53:11 +0530

    iniinit

---
+++ add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	int a,b,c;
+     printf ("Enter first number ");
+     scanf("%d",&a);
+     printf("enter secound number ");
+     scanf("%d",&b);
+    c=a+b;
+    printf("\n Sum of %d and %d =  %d\n",a,b,c);
+	return 0;
+}
---
+++ subtract.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+ int a ,b,c;
+ printf ("Enter First number ");
+ scanf("%d",&a);
+ printf("Enter secound number");
+ scanf("%d",&b);
+c=a-b;
+printf("\n %d\n",c);
+
+
+    return 0;
+}
